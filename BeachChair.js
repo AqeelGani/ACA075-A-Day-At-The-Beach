88eaa6dc9e20da9e98e679bedf13854ce2c4817e
AqeelGani <88658847+AqeelGani@users.noreply.github.com>
2021-12-25 14:41:30 +0530

    Add files via upload

---
+++ BeachChair.js
@@ -0,0 +1,6 @@
+class BeachChair extends BaseClass {
+    constructor(x, y) {
+        super(x, y, 150, 150);
+        this.image = loadImage("./sprites/beach-chair.png");
+    }
+}
---
+++ PicnicBasket.js
@@ -0,0 +1,6 @@
+class PicnicBasket extends BaseClass {
+    constructor(x, y) {
+        super(x, y, 100, 100);
+        this.image = loadImage("./sprites/picnic-basket.png");
+    }
+}
